d632ebca2c022db36c1066bbd35b0c366cd7504a
Alex Wendland <me@alexwendland.com>
2013-07-23 00:23:16 -0700

    Functionality of non-WarpPortals is now toggleable
    Signed-off-by: Alex Wendland <me@alexwendland.com>

--- src/com/mccraftaholics/warpportals/helpers/Defaults.java
+++ src/com/mccraftaholics/warpportals/helpers/Defaults.java
@@ -11,4 +11,6 @@ public class Defaults {
 	public static final String TP_MESSAGE = "Wooooooosh!";
 	public static final String TP_MSG_COLOR = "§d";
 
+	public static final boolean ALLOW_NORMAL_PORTALS = false;
+
 }
