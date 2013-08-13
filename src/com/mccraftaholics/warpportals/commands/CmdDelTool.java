e48e9c11b4f99108921161a51fa1c2ccbc7d10ff
Alex Wendland <me@alexwendland.com>
2013-08-13 10:05:51 -0700

    Send user a message when DelTool equipped

--- src/com/mccraftaholics/warpportals/commands/CmdDelTool.java
+++ src/com/mccraftaholics/warpportals/commands/CmdDelTool.java
@@ -15,6 +15,7 @@ public class CmdDelTool {
 				ItemStack curItem = player.getItemInHand();
 				if (!curItem.getType().isBlock()) {
 					main.mPortalManager.addDeleting(player.getName(), curItem.getType());
+					sender.sendMessage(main.mCC + "Portal delete tool equipped to \"" + curItem.getType() + "\"");
 				} else
 					sender.sendMessage(main.mCC + "You can't use a block for that! Try using something like the fishing rod.");
 			} catch (Exception e) {
