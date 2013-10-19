3831ae75cab6e1ab24f295fc932dfa1f0ad21d2f
Alex Wendland <me@alexwendland.com>
2013-10-19 09:30:24 -0700

    Better commenting

--- src/com/mccraftaholics/warpportals/bukkit/BukkitEventListener.java
+++ src/com/mccraftaholics/warpportals/bukkit/BukkitEventListener.java
@@ -19,61 +19,69 @@ import com.mccraftaholics.warpportals.manager.PortalManager;
 import com.mccraftaholics.warpportals.objects.CoordsPY;
 
 public class BukkitEventListener implements Listener {
-	JavaPlugin mPlugin;
-	PortalManager mPortalManager;
-	YamlConfiguration mPortalConfig;
-	ChatColor mCC;
-	String mTPMessage;
-	ChatColor mTPC;
-	boolean mAllowNormalPortals;
+    JavaPlugin mPlugin;
+    PortalManager mPortalManager;
+    YamlConfiguration mPortalConfig;
+    ChatColor mCC;
+    String mTPMessage;
+    ChatColor mTPC;
+    boolean mAllowNormalPortals;
 
-	public BukkitEventListener(JavaPlugin plugin, PortalManager portalManager, YamlConfiguration portalConfig) {
-		mPlugin = plugin;
-		mPortalManager = portalManager;
-		mPortalConfig = portalConfig;
+    public BukkitEventListener(JavaPlugin plugin, PortalManager portalManager, YamlConfiguration portalConfig) {
+        mPlugin = plugin;
+        mPortalManager = portalManager;
+        mPortalConfig = portalConfig;
 
-		mCC = ChatColor.getByChar(mPortalConfig.getString("portals.general.textColor", Defaults.CHAT_COLOR));
-		mTPMessage = mPortalConfig.getString("portals.teleport.message", Defaults.TP_MESSAGE);
-		mTPC = ChatColor.getByChar(mPortalConfig.getString("portals.teleport.messageColor", Defaults.TP_MSG_COLOR));
-		mAllowNormalPortals = mPortalConfig.getBoolean("portals.general.allowNormalPortals", Defaults.ALLOW_NORMAL_PORTALS);
-	}
+        mCC = ChatColor.getByChar(mPortalConfig.getString("portals.general.textColor", Defaults.CHAT_COLOR));
+        mTPMessage = mPortalConfig.getString("portals.teleport.message", Defaults.TP_MESSAGE);
+        mTPC = ChatColor.getByChar(mPortalConfig.getString("portals.teleport.messageColor", Defaults.TP_MSG_COLOR));
+        mAllowNormalPortals = mPortalConfig.getBoolean("portals.general.allowNormalPortals", Defaults.ALLOW_NORMAL_PORTALS);
+    }
 
-	long mSartTime = 0;
-	long mTimeTaken = 0;
+    long mSartTime = 0;
+    long mTimeTaken = 0;
 
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onPortalEnter(PlayerPortalEvent event) {
-		Player player = event.getPlayer();
-		CoordsPY tpCoords = mPortalManager.checkPlayerLoose(player.getLocation());
-		if (tpCoords != null) {
-			if (player.hasPermission("warpportal.enter")) {
-				// So event doesn't propagate to default handling
-				event.setCancelled(true);
-				// Handle WarpPortal Teleportation
-				player.sendMessage(mTPC + mTPMessage);
-				Location tpLoc = new Location(tpCoords.world, tpCoords.x, tpCoords.y, tpCoords.z);
-				tpLoc.setPitch(tpCoords.pitch);
-				tpLoc.setYaw(tpCoords.yaw);
-				player.teleport(tpLoc);
-			}
-		} else {
-			if (!mAllowNormalPortals)
-				event.setCancelled(true);
-		}
-	}
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPortalEnter(PlayerPortalEvent event) {
+        // Get player involved in the event
+        Player player = event.getPlayer();
+        // Check if player is in a WarpPortal or normal portal
+        /* Must be one of the two because this is triggered on the Bukkit PortalEnter event */
+        CoordsPY tpCoords = mPortalManager.checkPlayerLoose(player.getLocation());
+        // If the player entered a WarpPortal
+        if (tpCoords != null) {
+            // Check player permissions to use the portal
+            if (player.hasPermission("warpportal.enter")) {
+                // Cancel event so that it doesn't propagate to default handling
+                event.setCancelled(true);
+                /* Handle WarpPortal teleportation */
+                player.sendMessage(mTPC + mTPMessage);
+                Location tpLoc = new Location(tpCoords.world, tpCoords.x, tpCoords.y, tpCoords.z);
+                tpLoc.setPitch(tpCoords.pitch);
+                tpLoc.setYaw(tpCoords.yaw);
+                player.teleport(tpLoc);
+            }
+        } else {
+            // The player did not enter a WarpPortal
+            // Check to see if the server is configured to allow normal portal events
+            if (!mAllowNormalPortals)
+                // If not allowed, cancel the event
+                event.setCancelled(true);
+        }
+    }
 
-	@EventHandler
-	public void onPlayerInteract(PlayerInteractEvent e) {
-		if (e.hasBlock() && e.hasItem() && e.getClickedBlock() != null && e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-			mPortalManager.playerItemRightClick(e);
-		}
-	}
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e) {
+        if (e.hasBlock() && e.hasItem() && e.getClickedBlock() != null && e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            mPortalManager.playerItemRightClick(e);
+        }
+    }
 
-	@EventHandler
-	public void o(BlockPhysicsEvent e) {
-		if (e.getBlock().getType() == Material.PORTAL) {
-			e.setCancelled(true);
-		}
-	}
+    @EventHandler
+    public void o(BlockPhysicsEvent e) {
+        if (e.getBlock().getType() == Material.PORTAL) {
+            e.setCancelled(true);
+        }
+    }
 
 }
