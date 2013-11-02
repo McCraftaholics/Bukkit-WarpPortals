d098d4023e95890a2e0a24164c229906a3c3d029
Alex Wendland <me@alexwendland.com>
2013-11-02 15:02:17 -0700

    Check permission before executing List WarpPortals

--- src/com/mccraftaholics/warpportals/commands/CmdPortalList.java
+++ src/com/mccraftaholics/warpportals/commands/CmdPortalList.java
@@ -9,27 +9,29 @@ import com.mccraftaholics.warpportals.objects.PortalInfo;
 
 public class CmdPortalList extends CommandHandlerObject {
 	public static boolean handle(CommandSender sender, String[] args, CommandHandler main) {
-		StringBuilder sblist = new StringBuilder();
-		sblist.append("Portals:");
-		for (String portalName : main.mPortalManager.getPortalNames()) {
-			// Retrieve Portal Info
-			PortalInfo portalInfo = main.mPortalManager.getPortalInfo(portalName);
-			// Retrieve linked Teleport Destination
-			String destText = main.mPortalManager.getDestinationName(portalInfo.tpCoords);
-			/*
-			 * If the teleport destination does not have a name, show the
-			 * coordinates
-			 */
-			if (destText == null)
-				destText = portalInfo.tpCoords.toNiceString();
-			try {
-				sblist.append(ChatColor.WHITE + "\n - " + ChatColor.RED + portalName + ChatColor.YELLOW + " (" + portalInfo.blockCoordArray.get(0).world.getName() + ") "
-						+ ChatColor.WHITE + "-> " + ChatColor.AQUA + destText);
-			} catch (Exception e) {
-				// Catches exceptions when blockCoordArray is 0 in length
+		if (sender.hasPermission("warpportals.admin.destination.list")) {
+			StringBuilder sblist = new StringBuilder();
+			sblist.append("Portals:");
+			for (String portalName : main.mPortalManager.getPortalNames()) {
+				// Retrieve Portal Info
+				PortalInfo portalInfo = main.mPortalManager.getPortalInfo(portalName);
+				// Retrieve linked Teleport Destination
+				String destText = main.mPortalManager.getDestinationName(portalInfo.tpCoords);
+				/*
+				 * If the teleport destination does not have a name, show the
+				 * coordinates
+				 */
+				if (destText == null)
+					destText = portalInfo.tpCoords.toNiceString();
+				try {
+					sblist.append(ChatColor.WHITE + "\n - " + ChatColor.RED + portalName + ChatColor.YELLOW + " ("
+							+ portalInfo.blockCoordArray.get(0).world.getName() + ") " + ChatColor.WHITE + "-> " + ChatColor.AQUA + destText);
+				} catch (Exception e) {
+					// Catches exceptions when blockCoordArray is 0 in length
+				}
 			}
+			sender.sendMessage(main.mCC + sblist.toString());
 		}
-		sender.sendMessage(main.mCC + sblist.toString());
 		return true;
 	}
 }
