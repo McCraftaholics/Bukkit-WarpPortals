a0d4c552d0ee6eb0d9b275374292412bf5586901
Alex Wendland <me@alexwendland.com>
2013-12-28 13:21:39 -0800

    Update code commentary to reflect what this class does

--- src/com/mccraftaholics/warpportals/api/WarpPortalsEvent.java
+++ src/com/mccraftaholics/warpportals/api/WarpPortalsEvent.java
@@ -9,44 +9,49 @@ import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 /**
- *
+ * 
  * An Event that fires when a player is teleported by a WarpPortal
- *
+ * 
  * @see Event
- *
+ * 
  */
 public class WarpPortalsEvent extends Event implements Cancellable {
 
 	private static final HandlerList handlers = new HandlerList();
 	private boolean cancelled;
 
-    // Event data
-    private Player player;
-    private PortalInfo portal;
-    private boolean hasPermission;
+	// Event data
+	private Player player;
+	private PortalInfo portal;
+	private boolean hasPermission;
 
-    // Modifiable event data
-    private CoordsPY tpCoords;
+	// Modifiable event data
+	private CoordsPY tpCoords;
 
 	/**
-	 * Constructor of a WorldSniperWandSelectionEvent * * Between pos1 and pos2
-	 * is the selection *
-	 *
+	 * Constructor of a WarpPortalsEvent, triggered when a player is teleported
+	 * by a WarpPortal
+	 * 
 	 * @param p
-	 *            - Player that is selecting Something WIth the wand * @param
-	 *            pos1 - The First Position
+	 *            - Player that is getting teleported
+	 * @param po
+	 *            - PortalInfo for the WarpPortal that the player has entered.
+	 *            Contains the WarpPortal's name, blocks, destination coords.
+	 * @param hp
+	 *            - boolean if the player has permission to be teleported or
+	 *            not.
 	 */
 	public WarpPortalsEvent(Player p, PortalInfo po, boolean hp) {
 		this.cancelled = !hp;
 
-        player = p;
-        portal = po.clone();
-        hasPermission = hp;
+		player = p;
+		portal = po.clone();
+		hasPermission = hp;
 
-        tpCoords = portal.tpCoords.clone();
+		tpCoords = portal.tpCoords.clone();
 	}
 
-    /* Bukkit Event API methods */
+	/* Bukkit Event API methods */
 
 	@Override
 	public HandlerList getHandlers() {
@@ -67,65 +72,71 @@ public class WarpPortalsEvent extends Event implements Cancellable {
 		this.cancelled = cancel;
 	}
 
+	/* WarpPortal Event API methods */
 
-    /* WarpPortal Event API methods */
+	/* Get event data */
+	/**
+	 * Get the player that is entering the WarpPortal.
+	 * 
+	 * @return Player associated with the event
+	 */
+	public Player getPlayer() {
+		return player;
+	}
 
-    /* Get event data */
-    /**
-     * Get the player that is entering the WarpPortal.
-     *
-     * @return Player associated with the event
-     */
-    public Player getPlayer() {
-        return player;
-    }
+	/**
+	 * Get a copy of the WarpPortal that the player is entering. PortalInfo
+	 * contains portal location data and teleportation data.
+	 * 
+	 * @return PortalInfo - WarpPortal associated with the event
+	 */
+	public PortalInfo getPortal() {
+		return portal;
+	}
 
-    /**
-     * Get a copy of the WarpPortal that the player is entering. PortalInfo contains portal location data and teleportation data.
-     *
-     * @return PortalInfo - WarpPortal associated with the event
-     */
-    public PortalInfo getPortal() {
-        return portal;
-    }
+	/**
+	 * Does the player have permission to enter the portal? Permission according
+	 * to player.hasPermission("warpportal.enter")
+	 * 
+	 * @return boolean
+	 */
+	public boolean hasPermission() {
+		return hasPermission;
+	}
 
-    /**
-     * Does the player have permission to enter the portal? Permission according to player.hasPermission("warpportal.enter")
-     *
-     * @return boolean
-     */
-    public boolean hasPermission() {
-        return hasPermission;
-    }
+	/**
+	 * Get a copy of the Teleport Coordinates that the player is scheduled for.
+	 * CoordsPY contains world, x, y, z, pitch and yaw data.
+	 * 
+	 * @return CoordsPY - Teleport CoordsPY associated with the event
+	 */
+	public CoordsPY getTeleportCoordsPY() {
+		return tpCoords;
+	}
 
-    /**
-     * Get a copy of the Teleport Coordinates that the player is scheduled for. CoordsPY contains world, x, y, z, pitch and yaw data.
-     *
-     * @return CoordsPY - Teleport CoordsPY associated with the event
-     */
-    public CoordsPY getTeleportCoordsPY() {
-        return tpCoords;
-    }
+	/* Set event data */
 
-    /* Set event data */
+	/**
+	 * Modify the player's teleport destination. The CoordsPY supplied to this
+	 * method will override the original teleport destination, and WarpPortals
+	 * will teleport the player to this new location.
+	 * 
+	 * @param newTPC
+	 *            - CoordsPY of the new destination
+	 */
+	public void setTeleportCoordsPY(CoordsPY newTPC) {
+		tpCoords = newTPC;
+	}
 
-    /**
-     * Modify the player's teleport destination.
-     * The CoordsPY supplied to this method will override the original teleport destination, and WarpPortals will teleport the player to this new location.
-     *
-     * @param newTPC - CoordsPY of the new destination
-     */
-    public void setTeleportCoordsPY(CoordsPY newTPC) {
-        tpCoords = newTPC;
-    }
-
-    /**
-     * Modify the player's teleport destination.
-     * The Location supplied to this method will override the original teleport destination, and WarpPortals will teleport the player to this new location.
-     *
-     * @param newLoc - CoordsPY of the new destination
-     */
-    public void setTeleportCoordsPY(Location newLoc) {
-        setTeleportCoordsPY(new CoordsPY(newLoc));
-    }
+	/**
+	 * Modify the player's teleport destination. The Location supplied to this
+	 * method will override the original teleport destination, and WarpPortals
+	 * will teleport the player to this new location.
+	 * 
+	 * @param newLoc
+	 *            - CoordsPY of the new destination
+	 */
+	public void setTeleportCoordsPY(Location newLoc) {
+		setTeleportCoordsPY(new CoordsPY(newLoc));
+	}
 }
